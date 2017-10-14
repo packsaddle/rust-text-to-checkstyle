0514ad7e10f8b6ad9611a222861315df95112b61
Sanemat <o.gata.ken@gmail.com>
2017-10-14 09:49:45 +0900

    feat(lib): copy run method

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+use std::error::Error;
+mod checkstyle;
+
+pub fn run(
+    message: &str,
+    column: u32,
+    line: u32,
+    severity: &str,
+    source: &str,
+    name: &str,
+) -> Result<String, Box<Error>> {
+    let piece = checkstyle::ErrorPiece {
+        column,
+        line,
+        message: message.to_string(),
+        severity: severity.to_string(),
+        source: source.to_string(),
+    };
+    let file = checkstyle::ErrorFile {
+        name: name.to_string(),
+        error_pieces: vec![piece],
+    };
+    let container = checkstyle::Container { error_files: vec![file] };
+    return container.to_xml();
 }
